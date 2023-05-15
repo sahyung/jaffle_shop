e7015728b089ad8054b2dbdd9bc223eab3bdc94e
sahyung <ende.astro@gmail.com>
2023-05-15 14:51:06 +0700

    use cast

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -11,7 +11,7 @@ with source as (
 renamed as (
 
     select
-        {{ database }} as db_source,
+        cast({{ custom_variable }} as {{ dbt_utils.type_string() }}) as db_source,
         id as customer_id,
         first_name,
         last_name
